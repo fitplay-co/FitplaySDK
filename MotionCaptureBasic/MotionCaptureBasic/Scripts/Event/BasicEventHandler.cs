2039a5b4056507340b980e0a50681a0866bda693
DONGYIHUI-PC\dongyihui <polaris7765@qq.com>
2022-06-21 11:25:38 +0800

    增加名字空间

--- MotionCaptureBasic/MotionCaptureBasic/Scripts/Event/BasicEventHandler.cs
+++ MotionCaptureBasic/MotionCaptureBasic/Scripts/Event/BasicEventHandler.cs
@@ -1,16 +1,17 @@
 ﻿using MotionCaptureBasic.OSConnector;
 using System;
-using UnityEngine;
 using UnityEngine.Events;
-
-public static class BasicEventHandler
+namespace MotionCaptureBasic.OSConnector
 {
-    [Serializable] public class UEvent : UnityEvent { }
-    /// <summary>
-    ///订阅IMU数据
-    /// </summary>
-    public delegate void EventImuDataRecievied(string imuMessage);
-    public static EventImuDataRecievied OnImuDataRecieved;
+    public static class BasicEventHandler
+    {
+        [Serializable] public class UEvent : UnityEvent { }
+        /// <summary>
+        ///订阅IMU数据
+        /// </summary>
+        public delegate void EventImuDataRecievied(string imuMessage);
+        public static EventImuDataRecievied OnImuDataRecieved;
 
-    public static void DispatchImuDataEvent(string imuMessage) => OnImuDataRecieved?.Invoke(imuMessage);
+        public static void DispatchImuDataEvent(string imuMessage) => OnImuDataRecieved?.Invoke(imuMessage);
+    }
 }
