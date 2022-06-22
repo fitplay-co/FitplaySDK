d42614068d4f4ffe540297011178d048052a445b
DONGYIHUI-PC\dongyihui <polaris7765@qq.com>
2022-06-22 11:18:39 +0800

    增加动捕模式显示

--- MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/TestDemo/DisplayHud.cs
+++ MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/TestDemo/DisplayHud.cs
@@ -9,41 +9,32 @@ namespace StandTravelModel.TestDemo
         public void OnGUI()
         {
             GUIStyle labelStyle = new GUIStyle("label");
-            labelStyle.fontSize = 60;
+            labelStyle.fontSize = 30;
             labelStyle.normal.textColor = Color.red;
-            
-            GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.2f, 300, 80), "步频:" + $"{standTravelModelManager.currentFrequency:0.00}", labelStyle);
+            GUI.Label(new Rect(20, 40, 300, 40), (standTravelModelManager.Enabled ? "动捕模式": "非动捕模式"), labelStyle);
+            GUI.Label(new Rect(20, 160, 300, 40), "步频:" + $"{standTravelModelManager.currentFrequency:0.00}", labelStyle);
             
             if (standTravelModelManager.currentLeg < -0.5)
             {
-                GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.1f, 300, 80), "右脚", labelStyle);
+                GUI.Label(new Rect(20, 80, 300, 40), "右脚", labelStyle);
             }
             else if (standTravelModelManager.currentLeg > 0.5)
             {
-                GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.1f, 300, 80), "左脚", labelStyle);
+                GUI.Label(new Rect(20, 80, 300, 40), "左脚", labelStyle);
             }
             else
             {
-                GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.1f, 300, 80), "没抬脚", labelStyle);
+                GUI.Label(new Rect(20, 80, 300, 40), "没抬脚", labelStyle);
             }
 
             switch (standTravelModelManager.currentMode)
             {
                 case MotionMode.Stand:
-                    GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.3f, 300, 80), "Stand模式", labelStyle);
+                    GUI.Label(new Rect(20, 120, 300, 40), "Stand模式", labelStyle);
                     break;
                 case MotionMode.Travel:
-                    GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.3f, 300, 80), "Travel模式", labelStyle);
+                    GUI.Label(new Rect(20, 120, 300, 40), "Travel模式", labelStyle);
                     break;
-            }
-
-            if (standTravelModelManager.isJump)
-            {
-                GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.4f, 300, 80), "有起跳", labelStyle);
-            }
-            else
-            {
-                GUI.Label(new Rect(Screen.width * 0.1f, Screen.height * 0.4f, 300, 80), "没起跳", labelStyle);
             }
         }
     }
