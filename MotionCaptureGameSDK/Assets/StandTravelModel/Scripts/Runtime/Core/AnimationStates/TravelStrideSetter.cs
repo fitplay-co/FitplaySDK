9ce239c1ff573d2c78897ca6d5c2f7012b17478d
YANXUEFEI-PC\yanxuefei <yanxuefei@W5pm>
2022-07-21 15:02:41 +0800

    Fixed Strider Controling

--- MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/Core/AnimationStates/TravelStrideSetter.cs
+++ MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/Core/AnimationStates/TravelStrideSetter.cs
@@ -20,11 +20,21 @@ public class TravelStrideSetter : ITravelStrideSetter
         actionDetectionItem = travelOwner.selfMotionDataModel.GetActionDetectionData();
         if(actionDetectionItem != null && actionDetectionItem.walk != null)
         {
-            var leftStepLength = actionDetectionItem.walk.leftStepLength * 0.7f;
-            var rightStepLength = actionDetectionItem.walk.rightStepLength * 0.7f;
+            /* var leftStepLength = actionDetectionItem.walk.leftStepLength * 0.7f;
+            var rightStepLength = actionDetectionItem.walk.rightStepLength * 0.7f; */
 
             //striderBiped.SpeedScale = Mathf.Max(leftStepLength, 0.25f);
             //striderBiped.SpeedScale = 1;
+            if(actionDetectionItem.walk.leftLeg == 1)
+            {
+                striderBiped.SpeedScale = GetStride(actionDetectionItem.walk.leftHipAng);
+            }
         }
+    }
+
+    private float GetStride(float hipAngle)
+    {
+        var stride = (180f - hipAngle) / 60;
+        return Mathf.Max(0.3f, stride);
     }
 }
