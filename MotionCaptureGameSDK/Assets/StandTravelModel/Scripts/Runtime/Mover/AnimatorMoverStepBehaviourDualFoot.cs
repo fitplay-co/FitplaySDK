c21e4299231026f34da058273a5f0924c36ef300
YANXUEFEI-PC\yanxuefei <yanxuefei@W5pm>
2022-07-26 18:41:27 +0800

    Fixed Move Sliding

--- MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/Mover/AnimatorMoverStepBehaviourDualFoot.cs
+++ MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/Mover/AnimatorMoverStepBehaviourDualFoot.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class AnimatorMoverStepBehaviourDualFoot : AnimatorMoverStepBehaviour
 {
+    [SerializeField] private float progressLeftStart;
+    [SerializeField] private float progressLeftEnd;
+
     protected override IAnimatorMoverBiped CreateAnimatorMover(Animator animator)
     {
-        return new AnimatorMoverBiped(animator.transform);
+        return new AnimatorMoverBipedStepProgress(animator.transform, progressLeftStart, progressLeftEnd);
     }
 }
--- MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/Mover/MoverInners/AnimatorMoverBipedFrameUpdate.cs
+++ MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/Mover/MoverInners/AnimatorMoverBipedFrameUpdate.cs
@@ -1,30 +1,35 @@
 using UnityEngine;
 
-public class AnimatorMoverBipedFrameUpdate : AnimatorMoverBiped
+public class AnimatorMoverBipedStepProgress : AnimatorMoverBiped
 {
     private float leftFootStart;
     private float leftFootEnd;
+    private int animIdStepProgress;
+    private Animator animator;
 
-    public AnimatorMoverBipedFrameUpdate(Transform transform, float leftFootStart, float leftFootEnd) : base(transform)
+    public AnimatorMoverBipedStepProgress(Transform transform, float leftFootStart, float leftFootEnd) : base(transform)
     {
         this.leftFootStart = leftFootStart;
         this.leftFootEnd = leftFootEnd;
+        this.animator = transform.GetComponent<Animator>();
+        this.animIdStepProgress = Animator.StringToHash("stepProgress");
     }
 
     protected override int GetTouchingFoot(AnimatorStateInfo stateInfo)
     {
         var isLeft = false;
-        var normalizedTime = stateInfo.normalizedTime;
-        normalizedTime -= (int)normalizedTime;
+        var stepProgress = animator.GetFloat(animIdStepProgress);
+        stepProgress -= (int)stepProgress;
 
         if(leftFootStart < leftFootEnd)
         {
-            isLeft = normalizedTime <= leftFootEnd && normalizedTime >= leftFootStart;
+            isLeft = stepProgress <= leftFootEnd && stepProgress >= leftFootStart;
         }
         else
         {
-            isLeft = normalizedTime >= leftFootEnd && normalizedTime <= leftFootStart;
+            isLeft = stepProgress >= leftFootEnd && stepProgress <= leftFootStart;
         }
+
         return isLeft ? footIndexLeft : footIndexRight;
     }
 }
