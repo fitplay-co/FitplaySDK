7f8381ab1e94913899a33873efedb9d2c3bc0d7a
YANXUEFEI-PC\yanxuefei <yanxuefei@W5pm>
2022-08-09 15:01:38 +0800

    Fixed Step Progress

--- MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/Core/AnimationStates/Components/StepProgressCacher.cs
+++ MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/Core/AnimationStates/Components/StepProgressCacher.cs
@@ -65,13 +65,13 @@ namespace StandTravelModel.Scripts.Runtime.Core.AnimationStates.Components
         private float ConvertHipAngleToProgress(float angle)
         {
             //return Mathf.Max((180f - angle) / 90f, 0.2f);
-            return Mathf.Max(180f - angle) / 120f;
+            return Mathf.Clamp01(Mathf.Max(180f - angle) / 90f);
         }
 
         private float ConverHipAngleToProgressDown(float angle)
         {
             //return (angle - 120f) / 120f;
-            return 1 - (180f - angle) / 120f;
+            return Mathf.Clamp01(1f - (180f - angle) / 90f);
         }
     }
 }
