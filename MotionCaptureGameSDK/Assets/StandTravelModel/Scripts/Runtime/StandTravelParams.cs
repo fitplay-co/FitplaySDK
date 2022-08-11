929a5aefc22e38d7f1fa7ff75295e33f3fe132a6
YANXUEFEI-PC\yanxuefei <yanxuefei@W5pm>
2022-08-11 15:46:34 +0800

    Added Files Serializable Parameters

---
+++ MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/StandTravelParams.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class StandTravelParams
+{
+    public float runThrehold = 0.25f;
+    public bool useFrequency = true;
+}
---
+++ MotionCaptureGameSDK/Assets/StandTravelModel/Scripts/Runtime/StandTravelParamsLoader.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class StandTravelParamsLoader
+{
+    [SerializeField] private StandTravelParams standTravelParams;
+
+    private const string fileName = "/standTravelParams.json";
+
+    public void Deserialize()
+    {
+        standTravelParams = JsonHelper.Deserialize<StandTravelParams>(GetFilePath());
+        if(standTravelParams == null)
+        {
+            standTravelParams = new StandTravelParams();
+        }
+    }
+
+    public void Serialize()
+    {
+        JsonHelper.Serialize<StandTravelParams>(GetFilePath(), standTravelParams);
+    }
+
+    public float GetRunThrehold()
+    {
+        return standTravelParams.runThrehold;
+    }
+
+    public void SetRunThrehold(float value)
+    {
+        standTravelParams.runThrehold = value;
+    }
+
+    public bool GetUseFrequency()
+    {
+        return standTravelParams.useFrequency;
+    }
+
+    public void SetUseFrequency(bool value)
+    {
+        standTravelParams.useFrequency = value;
+    }
+
+    private string GetFilePath()
+    {
+        return Application.persistentDataPath + fileName;
+    }
+}
